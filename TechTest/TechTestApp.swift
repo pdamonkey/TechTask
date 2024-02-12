404214f26ab1438a92e0ceb730dfb5ee5c08d838
PDA Monkey <mail@pdamonkey.com>
2024-02-12 16:12:45 +0000

    Initial Commit

---
+++ TechTest/TechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechTestApp.swift
+//  TechTest
+//
+//  Created by Matthew Gallagher on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
